3f1d6823f3736d33d2909b7987750db3e8292839
DEV <dev@info.local>
2020-11-13 16:37:41 -0300

    Exercício 1 concluído

---
+++ Exercício - Condionais/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Exercício___Condionais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite sua idade");
+            int idade = int.Parse(Console.ReadLine());
+            //Processamento//
+            int meses = idade * 12;
+            int dias = idade * 365;
+            int horas = dias * 24;
+            int minutos = horas * 60;
+
+            //Apresentação 
+            Console.WriteLine("Você viveu");
+            Console.WriteLine(meses+" Meses");
+            Console.WriteLine(dias+ "Dias");
+            Console.WriteLine(horas+ "Horas");
+            Console.WriteLine(minutos+ "Minutos");
+
+            
+            
+            
+
+            
+            
+        }
+    }
+}
