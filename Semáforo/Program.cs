f9eda7b8cbb5c3fcc6d61b5a9357ac9a773f2b06
Matheus <mthneri@gmail.com>
2020-11-17 17:02:46 -0300

    Semáforo

---
+++ Semáforo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Semáforo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cor do semáforo: ");
+            Console.WriteLine("");
+            Console.WriteLine("Vermelho | Amarelo | Verde");
+            string resposta = Console.ReadLine();
+
+    
+    Console.WriteLine("");
+
+            switch (resposta){
+                case "Vermelho":
+                Console.WriteLine("Pare o veículo");
+                break;
+
+                case "Amarelo":
+                Console.WriteLine("Reduzir a velocidade");
+                break;
+
+                case "Verde":
+                Console.WriteLine("Acelere!");
+                break;
+
+                default:
+                Console.WriteLine("Este semáforo está com falha");
+                Console.WriteLine("");
+                Console.WriteLine("Não, agora preste atenção na sinalização");
+                Console.WriteLine("");
+                break;
+            }
+        }
+    }
+}
